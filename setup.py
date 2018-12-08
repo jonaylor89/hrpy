7de630449a58b2acba7252b31250869862f8b43a
John Naylor <jonaylor89@gmail.com>
2018-12-08 18:25:17 -0500

    Add console script:

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
   download_url = 'https://github.com/jonaylor89/hrpy/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['hr', 'terminal', 'formatting'],   # Keywords that define your package best
   install_requires=[],
+  entry_points={"console_scripts": ["glances=glances:main"]},
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
 
