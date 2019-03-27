8cff00711359f49ed25cbba11a830c47619fab08
John Naylor <jonaylor89@gmail.com>
2019-03-27 11:32:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 setup(
   name = 'hrpy',         # How you named your package folder (MyLib)
   packages = ['hrpy'],   # Chose the same as "name"
-  version = '0.0.8',      # Start with a small number and increase it with every change you make
+  version = '0.0.9',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'hr but written in python',   # Give a short description about your library
   long_description = README, 
